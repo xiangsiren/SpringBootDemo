2666dbe67d3bf2c10d8ceba13eeacc3588ebb65c
Xiaolu Chen <xiaolu.chen@cloudtogo.cn>
2018-01-26 14:10:28 +0800

    a little

--- src/main/java/com/example/demo/web/controller/IndexController.java
+++ src/main/java/com/example/demo/web/controller/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController {
             jsonObject.put("welcome", "hello, " + name + "!");
         }
         jsonObject.put("welcome1", "1");
-        jsonObject.put("welcome2", "2");
+        jsonObject.put("welcome3", "3");
         logger.info("result msg: {}", jsonObject.toJSONString());
         return jsonObject;
     }
